256606718e1e5fc52623d38f9bc84787153c5cb1
Max Sun <max.sun@yale.edu>
2019-05-16 06:31:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,9 +13,11 @@ def batch_badge_creator(names)
 end
 
 def assign_rooms(speakers)
-  speakers.each_with_index { |name, index|
-    return "Hello, #{name}! You'll be assigned to room #{index + 1}!"
-  }
+  for i in 1..speakers.size do
+    speakers.each_with_index { |name, index|
+      return "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+    }
+  end
 end
 
 def printer
